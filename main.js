a870ffe4fbd25a27286bb9939344cd8e1823d6b6
Scott C <78004109+curry-scott@users.noreply.github.com>
2021-02-10 21:08:12 -0500

    main.js

--- main.js
+++ main.js
@@ -7,23 +7,17 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-2. RAW TEXT STRINGS
+
 
 let storyText= "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day."
 
-let insertX = ['Willy the Goblin', 
-'Big Daddy', 
-'Father Christmas']
+let insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas']
 
-let insertY = ['the soup kitchen',
-'Disneyland',
-'the White House']
+let insertY = ['the soup kitchen', 'Disneyland', 'the White House']
 
-let insertZ = ['spontaneously combusted',
-'melted into a puddle on the sidewalk',
-'turned into a slug and crawled away']
+let insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away']
 
-3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
+
 
 randomize.addEventListener('click', result);
 
@@ -38,10 +32,11 @@ function result() {
     newStory = newStory.replace(':inserty:',yItem);
     newStory = newStory.replace(':insertz:',zItem);
     newStory = newStory.replace(':insertz:',zItem);
-        
+
 
   if(customName.value !== '') {
     let name = customName.value;
+    newStory = newStory.replace('Bob',name);
 
   }
 
@@ -49,8 +44,11 @@ function result() {
     let weight = Math.round(300);
     let temperature =  Math.round(94);
 
+    newStory = newStory.replace('300 pounds',weight);
+    newStory = newStory.replace('94 fahrenheit',temperature);
+
   }
 
-  story.textContent = ;
+  story.textContent = newStory;
   story.style.visibility = 'visible';
 }
